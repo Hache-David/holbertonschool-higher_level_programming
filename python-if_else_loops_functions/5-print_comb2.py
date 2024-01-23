adea344d99eef2f070c0e41daa7f0fd960c500dc
Hache-David <hache.david@free.fr>
2024-01-23 16:00:22 +0000

    Fix error on exercice 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for index in range(00, 99):
+for index in range(00, 100):
     if index < 10:
         print("0{}, ".format(index), end='')
     else:
-        print("{}{}".format(index, ", " if index != 98 else '\n'),  end='')
+        print("{}{}".format(index, ", " if index != 99 else '\n'),  end='')
