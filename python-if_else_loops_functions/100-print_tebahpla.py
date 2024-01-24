cb0f6c39f6344b38b2ae671564c3192dcd9a9347
Hache-David <hache.david@free.fr>
2024-01-24 15:46:57 +0000

    fix correct output on first advanced task

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,10 +4,8 @@ int = 122
 bool = True
 while int != 96:
     if bool is True:
-        print(chr(int), end='')
-        int -= 1
         bool = False
     else:
-        print(chr(int - 32), end='')
-        int -= 1
         bool = True
+    print(chr(int) if bool is True else chr(int - 32), end='')
+    int -= 1
