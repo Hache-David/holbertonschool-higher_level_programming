7cf8204589269ee1097916156ea9f3d5dfec3aea
Hache-David <hache.david@free.fr>
2024-01-24 13:07:15 +0000

    fix error

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
+# 7-islower.py
 
-'''def islower(c):
+def islower(c):
     if ord(c) in range(65, 91):
         return False
     elif ord(c) in range(97, 123):
         return True
     else:
         return False
-'''
-def islower(c):
-
-    if ord(c) >= 97 and ord(c) <= 122:
-        return False
-    else:
-        return True
