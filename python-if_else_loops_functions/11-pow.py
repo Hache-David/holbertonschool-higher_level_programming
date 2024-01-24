049b775d8de333c75da613b46c0f3e0d2a48c725
Hache-David <hache.david@free.fr>
2024-01-24 15:28:20 +0000

    Fix error

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    index = a
     if a == 0:
         return a
-    if b == 0:
-        a = 1
-        return a
-    if b < 0:
-        while b != 1:
-            a = a / index
-            b += 1
-    if b > 1:
-        while b != 1:
-            a = a * index
-            b -= 1
-    return a
+    else:
+        return a**b
