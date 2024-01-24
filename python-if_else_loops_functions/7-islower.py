25a3eb4817fc5a4b1a3fcf2c8fb723c770463f69
Hache-David <hache.david@free.fr>
2024-01-24 12:49:41 +0000

    fdrdv

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
-def islower(c):
+'''def islower(c):
     if ord(c) in range(65, 91):
         return False
     elif ord(c) in range(97, 123):
         return True
     else:
         return False
+'''
+def islower(c):
+
+    if ord(c) >= 97 and ord(c) <= 122:
+        return False
+    else:
+        return True
