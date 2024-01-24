3c5b4c87a92b8f13acbf6213e0003d6538679dc9
Hache-David <hache.david@free.fr>
2024-01-24 15:01:08 +0000

    Add file for exercice 11 with pycodestyle checked

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    index = a
+    if b == 0:
+        return 1
+    if b < 0:
+        while b != 1:
+            a = a / index
+            b += 1
+    else:
+        while b != 1:
+            a = a * index
+            b -= 1
+    return a
