2f15c76d1a980fbdb1c66325cc5e134b00b43cbe
Hache-David <hache.david@free.fr>
2024-01-25 14:28:50 +0000

    Fix requierement

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,7 +5,11 @@ import calculator_1 as calc
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, calc.add(a, b)))
-    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calc.div(a, b)))
+    add = calc.add(a, b)
+    sub = calc.sub(a, b)
+    mul = calc.mul(a, b)
+    div = calc.div(a, b)
+    print("{} + {} = {}".format(a, b, add))
+    print("{} - {} = {}".format(a, b, sub))
+    print("{} * {} = {}".format(a, b, mul))
+    print("{} / {} = {}".format(a, b, div))
