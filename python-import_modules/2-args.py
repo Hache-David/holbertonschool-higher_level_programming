dd63fa1a0f3c4d655aa393bd59ee261487e871c9
Hache-David <hache.david@free.fr>
2024-01-26 13:47:10 +0000

    fix error

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-import sys
 
-i = 1
-if len(sys.argv) == 2:
-    print("1 argument:")
-elif len(sys.argv) == 1:
-    print("0 arguments.".format(len(sys.argv) - 1))
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-while i != len(sys.argv):
-    print("{} : {}".format(i, sys.argv[i]))
-    i += 1
+if __name__ == "__main__":
+
+    import sys
+
+    i = 1
+    if len(sys.argv) == 2:
+        print("1 argument:")
+    elif len(sys.argv) == 1:
+        print("0 arguments.".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    while i != len(sys.argv):
+        print("{} : {}".format(i, sys.argv[i]))
+        i += 1
