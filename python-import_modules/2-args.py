32b40f98ac3868da4bcbbcf6fb8f8598022d6c83
Hache-David <hache.david@free.fr>
2024-01-26 13:45:53 +0000

    fix requierement

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 import sys
+
 i = 1
-print("{} arguments.".format(len(sys.argv) - 1))
+if len(sys.argv) == 2:
+    print("1 argument:")
+elif len(sys.argv) == 1:
+    print("0 arguments.".format(len(sys.argv) - 1))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
 while i != len(sys.argv):
     print("{} : {}".format(i, sys.argv[i]))
     i += 1
