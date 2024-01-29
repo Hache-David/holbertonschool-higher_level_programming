9cc16414a9d0ed18080abd72351d5f1a44632066
Hache-David <hache.david@free.fr>
2024-01-29 13:08:28 +0000

    exercice 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
+
+    import calculator_1 as calc
+
     a = 10
     b = 5
-    add = add(a, b)
-    sub = sub(a, b)
-    mul = mul(a, b)
-    div = div(a, b)
-    print("{} + {} = {}".format(a, b, add))
-    print("{} - {} = {}".format(a, b, sub))
-    print("{} * {} = {}".format(a, b, mul))
-    print("{} / {} = {}".format(a, b, div))
+    print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calc.div(a, b)))
