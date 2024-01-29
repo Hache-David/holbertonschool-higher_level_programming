4397448bb72b9d1362fccf09510a0c1672d971dc
Hache-David <hache.david@free.fr>
2024-01-29 13:32:18 +0000

    fix error

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,8 +4,8 @@ def element_at(my_list, idx):
     i = 0
     if idx < 0:
         return None
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return None
-    while i < idx:
+    while i != idx:
         i += 1
     return my_list[i]
