9212066ac47a3cf4b2aa0aaa26326cd6f19cda8d
Hache-David <hache.david@free.fr>
2024-01-29 16:08:40 +0000

    fix error

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    taille = 0
     for ligne in matrix:
-        print("{}".format(ligne))
+        taille = len(ligne) - 1
+        for int in ligne:
+            print("{}".format(int), end="")
+            if int != ligne[taille]:
+                print(" ", end="")
+        print()
