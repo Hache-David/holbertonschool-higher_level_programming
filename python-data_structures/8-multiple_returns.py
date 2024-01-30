449c8beaf21aa2e5b2c57bb7f214b033ebd30b35
Hache-David <hache.david@free.fr>
2024-01-30 15:21:59 +0000

    Fix error

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    tuples = (len(sentence), sentence[0])
+    tuples = ()
+    if len(sentence) == 0:
+        tuples = (0, None)
+    else:
+        tuples = (len(sentence), sentence[0])
     return tuples
