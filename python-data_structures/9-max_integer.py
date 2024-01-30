22b155fb608f491e14beea4ffa385b2ca6dc26c7
Hache-David <hache.david@free.fr>
2024-01-30 15:31:26 +0000

    fix error

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    idx = my_list[0]
+    idx = 0
     if len(my_list) == 0:
         return None
+    idx = my_list[0]
     for i in my_list:
         if i > idx:
             idx = i
