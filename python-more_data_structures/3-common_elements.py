75546729a2b37f08e1a28891b51c393cafd85a8e
Hache-David <hache.david@free.fr>
2024-01-31 13:36:30 +0000

    fix error

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
+    new_set = []
     for element in set_1:
         for element2 in set_2:
             if element == element2:
-                return element
-    return None
+                new_set.append(element)
+    return new_set
