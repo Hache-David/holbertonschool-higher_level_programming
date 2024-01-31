14d729f061c4a7e2ec6ee42a0e0344b79b99d186
Hache-David <hache.david@free.fr>
2024-01-31 13:34:41 +0000

    Add file for exercice 3

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    for element in set_1:
+        for element2 in set_2:
+            if element == element2:
+                return element
+    return None
