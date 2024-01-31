05e1588fc4eeb32f3aedfb7d277b6ca5c9049cb9
Hache-David <hache.david@free.fr>
2024-01-31 15:32:10 +0000

    Add exercice 11

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    index = 0
+    key_save = ""
+    if a_dictionary is None:
+        return None
+    for key in a_dictionary:
+        if a_dictionary[key] > index:
+            index = a_dictionary[key]
+            key_save = key
+    return key_save
