05407acc6b96484abc6dabb1b8386ed0999d671b
Hache-David <hache.david@free.fr>
2024-02-08 14:20:47 +0000

    Add exercice 4 with pycodestyle

---
+++ python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''defines a square '''
+
+
+class Square:
+    '''Super square'''
+
+    def __init__(self, size=0) -> None:
+        assert isinstance(size, int), "size must be an integer"
+        assert size >= 0, "size must be >= 0"
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
