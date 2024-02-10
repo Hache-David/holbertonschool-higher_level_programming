8eb15d58264ed2e61d88272900dd75059b3bfd02
Hache-David <hache.david@free.fr>
2024-02-10 15:21:14 +0000

    Add exercice 7 with pycode style checked

---
+++ python-classes/6-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+'''defines a square'''
+
+
+class Square:
+
+    '''Super square'''
+
+    def __init__(self, size=0, position=(0, 0)) -> None:
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        assert isinstance(value, int), "size must be an integer"
+        assert value >= 0, "size must be >= 0"
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        assert isinstance(
+            value, (int, int)), "position must be a tuple of 2 positive integers"
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size > 0:
+            for i in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(" ", end="")
+                for y in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print("")
