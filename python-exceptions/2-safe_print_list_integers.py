5250c1571845ac780032b9687f78cd5f66d14563
Hache-David <hache.david@free.fr>
2024-02-10 11:07:36 +0000

    Add new code structure, for resolve checker issue

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-    index = 0
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                index += 1
-            else:
-                i += 1
-    except IndexError:
-        pass
-    finally:
-        print()
-    return index
+    number = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            number += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (number)
