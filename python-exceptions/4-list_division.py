dd8b025990604b1c8f2b551f7069ca23b624324a
Hache-David <hache.david@free.fr>
2024-02-10 11:14:04 +0000

    Fix code error

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,12 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(0, list_length):
         try:
-            if isinstance(my_list_1[i], int) and isinstance(my_list_2[i], int):
-                if my_list_2[i] == 0:
-                    raise ZeroDivisionError
-                new_list.append(my_list_1[i] / my_list_2[i])
-            else:
-                raise TypeError
+            new_list.append(my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
             print("division by 0")
             new_list.append(0)
