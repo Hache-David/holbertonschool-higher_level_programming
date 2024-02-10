0f95cdcef3e19e9f861145a0413f1016cc4279f4
Hache-David <hache.david@free.fr>
2024-02-10 14:45:00 +0000

    Fix pycodestyle

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 '''defines a square'''
+
+
 class Square:
     '''Super square'''
+
     def __init__(self, size=0) -> None:
         self.__size = size
 
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, size=0):
         assert isinstance(size, int), "size must be an integer"
