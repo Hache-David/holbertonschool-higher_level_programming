b10e3a46042e976229c71838e6f20ea6f001d396
Hache-David <hache.david@free.fr>
2024-02-10 14:44:05 +0000

    Add exercice 5

---
+++ python-classes/4-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''defines a square'''
+class Square:
+    '''Super square'''
+    def __init__(self, size=0) -> None:
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, size=0):
+        assert isinstance(size, int), "size must be an integer"
+        assert size >= 0, "size must be >= 0"
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
