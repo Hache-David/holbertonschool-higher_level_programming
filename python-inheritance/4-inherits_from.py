cd42ed596c67e8676e1721a3214ea58dc406ad13
Hache_David <hache.david@free.fr>
2024-02-21 10:23:57 +0000

    Add exercice 6 with pycodestyle checked

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 '''function that returns True if the object is an instance of a class that
 inherited (directly or indirectly) from the specified class ; otherwise False'''
+
+
 def inherits_from(obj, a_class):
     '''super class'''
     if isinstance(obj, a_class):
@@ -9,4 +11,3 @@ def inherits_from(obj, a_class):
         else:
             return True
     return False
-        
---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Write an empty class BaseGeometry.'''
+
+
+class BaseGeometry:
+    def __init__(self) -> None:
+        pass
