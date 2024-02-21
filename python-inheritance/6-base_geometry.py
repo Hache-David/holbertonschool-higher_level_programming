3724ad6e359c9f821e298b5229055e1e9c340b1f
Hache_David <hache.david@free.fr>
2024-02-21 10:28:37 +0000

    Add exercice 7 with pycodestyle checked

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Write a class BaseGeometry (based on 5-base_geometry.py).'''
+
+
+class BaseGeometry:
+    def __init__(self) -> None:
+        pass
+
+    def area(self):
+        '''raise an exception'''
+        raise Exception("area() is not implemented")
