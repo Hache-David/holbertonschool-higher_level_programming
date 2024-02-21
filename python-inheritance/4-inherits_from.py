23609fe424d86ffee2a113c236d554485989ecb9
Hache_David <hache.david@free.fr>
2024-02-21 10:20:06 +0000

    Add exercice 5 for project 2

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''function that returns True if the object is an instance of a class that
+inherited (directly or indirectly) from the specified class ; otherwise False'''
+def inherits_from(obj, a_class):
+    '''super class'''
+    if isinstance(obj, a_class):
+        if type(obj) is a_class:
+            return False
+        else:
+            return True
+    return False
+        
