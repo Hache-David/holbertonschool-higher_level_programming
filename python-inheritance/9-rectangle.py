ee9a098ee62ccdf4bb0e397f3cc91db9d98491b7
Hache_David <hache.david@free.fr>
2024-02-21 11:18:14 +0000

    Add exercice 10 with pycodestyle checked

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Write a class Rectangle that inherits from
+BaseGeometry (7-base_geometry.py). (task based on 8-rectangle.py)'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    ''' Represent a rectangle using BaseGeometry.'''
+
+    def __init__(self, width, height) -> None:
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        str_return = f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
+        return str_return
+
+    def area(self):
+        return self.__height * self.__width
