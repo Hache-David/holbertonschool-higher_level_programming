c60253305ef2d4e1916d9168724bac4749c2283c
Hache_David <hache.david@free.fr>
2024-02-21 13:23:36 +0000

    Fix requierement

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,10 +4,12 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    '''Square'''
     def __init__(self, size) -> None:
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
+        '''AREA'''
         return self.__size * self.__size
