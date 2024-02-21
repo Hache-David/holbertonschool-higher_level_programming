aaad1cb2dc02c155054f912e7417a15ade0b647e
Hache_David <hache.david@free.fr>
2024-02-21 11:09:15 +0000

    Add exercice 9 with pycodestyle checked

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).'''
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    ''' Represent a rectangle using BaseGeometry.'''
+
+    def __init__(self, width, height) -> None:
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
