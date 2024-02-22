e731657d2c15a3b124f79e0a5b532bf2f08d2a98
Hache_David <hache.david@free.fr>
2024-02-22 13:42:45 +0000

    Fix requierement

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,7 @@ and returns the number of characters written'''
 
 
 def write_file(filename="", text=""):
+    '''write file'''
     with open(filename, 'w', encoding="utf-8") as file:
         write_data = file.write(text)
     return write_data
