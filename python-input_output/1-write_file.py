55ca035f31b8b4690887bd2b43b67aaec4af7767
Hache_David <hache.david@free.fr>
2024-02-22 13:40:40 +0000

    Add file for exercice 2 with pycodestyle

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Write a function that writes a string to a text file (UTF8)
+and returns the number of characters written'''
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding="utf-8") as file:
+        write_data = file.write(text)
+    return write_data
