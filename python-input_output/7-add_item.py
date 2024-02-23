f9cf79220303f07559767c38818b4bd762d7130c
Hache_David <hache.david@free.fr>
2024-02-23 13:17:26 +0000

    Fix import requierement

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 ''' a script that adds all arguments to a Python list, and then save them to a file'''
-import sys
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = "add_item.json"
 json_return = load_from_json_file(filename)
-json_return += sys.argv[1:]
+json_return += argv[1:]
 json_file = save_to_json_file(json_return, "add_item.json")
