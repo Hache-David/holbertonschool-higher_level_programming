216894a66b3d55ddf547ccf81111bd74db2fe867
Hache_David <hache.david@free.fr>
2024-02-23 12:31:11 +0000

    Fix requierement

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,6 +6,7 @@ import json
 
 
 def load_from_json_file(filename):
+    '''load from json file'''
     with open(filename, 'r', encoding="utf-8") as f:
         file_str = f.read()
     return json.loads(file_str)
