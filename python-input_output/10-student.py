d320c73f635ed99d00b5396fc0e4d5f2ce26ccf1
Hache_David <hache.david@free.fr>
2024-02-29 16:10:58 +0000

    Add exercice 11 with pycodestyle checked

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Write a class Student that defines a student '''
+
+
+class Student:
+    '''Student class'''
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''return class to json'''
+        new_dict = {}
+        if (isinstance(attrs, list) and
+                all(isinstance(element, str) for element in attrs)):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        else:
+            return self.__dict__
