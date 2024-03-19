724357deee3d6aa33167964dac5c464e48611481
Hache_David <hache.david@free.fr>
2024-03-19 09:55:15 +0000

    Add exercice 9 for first project and fix the correct name and id table.

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- creates a table called first_table in the current database in your MySQL server.
-USE mysql; CREATE TABLE first_table (ID INT, NAME VARCHAR(256));
+USE mysql; CREATE TABLE first_table (id INT, name VARCHAR(256));
--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 --  inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-USE mysql; INSERT INTO first_table (ID, NAME) VALUES(89, "Best School");
+USE mysql; INSERT INTO first_table (id, name) VALUES(89, "Best School");
