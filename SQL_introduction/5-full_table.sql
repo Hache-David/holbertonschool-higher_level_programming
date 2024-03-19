4a8f6635c70fa1b764b46f14b73f0bd1b8828a90
Hache_David <hache.david@free.fr>
2024-03-19 11:15:47 +0000

    Remove all 'USE database' command for correct usecase

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-USE mysql; SHOW COLUMNS FROM first_table;
+SHOW COLUMNS FROM first_table;
